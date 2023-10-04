3319a529fc241f25fc7086788a14bc6d71fcfbc1
Educorreia932 <skelozard1@gmail.com>
2023-10-04 13:38:46 +0100

    Moved example code to a Jupyter Notebook

--- flake.nix
+++ flake.nix
@@ -1,27 +1,30 @@
 {
-    description = "Python OpenCV Development Environment";
+  description = "Python OpenCV Development Environment";
 
-	inputs = {
-		nixpkgs = { 
-            url = "github:nixos/nixpkgs/nixos-unstable"; 
-        };
-        flake-utils = {
-            url = "github:numtide/flake-utils";
-        };
-	};
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
 
-	outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: 
-        let
-            pkgs = import nixpkgs {
-                inherit system;
-            };
-            pythonEnvironment = pkgs.python3.withPackages (ps: with ps; [ 
-                numpy
-                (opencv4.override { enableGtk2 = true; })
-            ]);
-        in
-        {
-            devShell = pythonEnvironment.env;
-        }
-    ); 
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      pythonEnvironment = pkgs.python3.withPackages (ps: with ps; [
+        jupyter
+        ipython
+        matplotlib
+        numpy
+        (opencv4.override { enableGtk2 = true; })
+      ]);
+    in
+    {
+      devShell = pythonEnvironment.env;
+    }
+  );
 }
