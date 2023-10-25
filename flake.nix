0e8aa845216d281df3a0707cc1baf6e03ae0107c
Educorreia932 <skelozard1@gmail.com>
2023-10-25 10:22:06 +0100

    Updated Nix flake

--- flake.nix
+++ flake.nix
@@ -2,20 +2,15 @@
   description = "Python OpenCV Development Environment";
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      pythonEnvironment = pkgs.python3.withPackages (ps: with ps; [
+      pkgs = import nixpkgs { inherit system; };
+      python = pkgs.python3;
+      pythonEnvironment = python.withPackages (ps: with ps; [
         jupyter
         ipython
         matplotlib
@@ -24,7 +19,9 @@
       ]);
     in
     {
-      devShell = pythonEnvironment.env;
+      devShell = pkgs.mkShell {
+        buildInputs = [ pythonEnvironment ];
+      };
     }
   );
 }
