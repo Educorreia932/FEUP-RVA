a6d8cfd1cdaf917ed579401e12cfa43fa549a31f
David Preda <dtpreda30@gmail.com>
2023-12-09 19:20:28 +0000

    Add score calculation

--- Ennichi/Assets/Scripts/Target.cs
+++ Ennichi/Assets/Scripts/Target.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public Transform targetTransform;
+    public Transform targetCenter;
+    public Transform targetEdge;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,15 @@ public class Target : MonoBehaviour
 		if (collision.collider.tag == "Dart") {
 			Destroy(collision.collider.GetComponent<Rigidbody>());
             collision.collider.transform.eulerAngles = targetTransform.eulerAngles;
+            float radius = Vector3.Distance(targetCenter.position, targetEdge.position);
+            float hitRadius = Vector3.Distance(targetCenter.position, collision.collider.transform.position);
+            float ratio = hitRadius / radius;
+            if (ratio >= 1.0f) {
+                Debug.Log("Outside");
+            } else {
+                int score =  (int) -(10.0f * (ratio - 1.0f));
+                Debug.Log(score);
+            }
 		}
 	}
 }
